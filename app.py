5dbfbaec4d02f4058fe5cd5e5089ffce19d81409
cbkatiaa <katiaaacb@gmail.com>
2024-05-31 11:19:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,10 @@ def iqindportero(df, j1):
         data_color = [(x - normmin) / (normmax - normmin) for x in data_color]
         cmap = LinearSegmentedColormap.from_list('rg', ["darkred", "red", "salmon", "yellowgreen", "green", "darkgreen"], N=256)
         cmap_invertida = LinearSegmentedColormap.from_list('rg', ["darkgreen", "green", "yellowgreen", "salmon", "red", "darkred"], N=256)
-        colors = cmap(data_color) if title != 'Pases hacia peligro %' else cmap_invertida(data_color)
+        if bar_data != 'Pases hacia peligro %':
+            colors = cmap(data_color)
+        else:
+            colors = cmap_invertida(data_color)
         ax.barh(x, y, color=colors, zorder=2, edgecolor='none')
         for c in ax.containers:
             labels = [(y * 100).astype(int) if y > .05 else "" for y in c.datavalues]
