3d916f2001b0e087e11b05d74c3f0e9492635d32
cbkatiaa <katiaaacb@gmail.com>
2024-06-11 11:52:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -40,20 +40,21 @@ def iqindportero(df, j1, pos):
 
     plt.rcParams["font.family"] = "Century Gothic"
 
-    df['Long balls total'] = (df['Long Balls'] / df['Long Ball%']) * 100
-    df['Long balls per pass'] = df['Long balls total'] / df['OP Passes']
-    df['% pases son largos'] = df['Long balls per pass'].rank(pct=True)
-    df['Goles parados'] = df['GSAA'].rank(pct=True)
-    df['OBV portero'] = df['Goalkeeper OBV'].rank(pct=True)
-    df['Salidas de libero del portero'] = df['GK Aggressive Dist.'].rank(pct=True)
-    df['Salidas de portero (centros)'] = df['Claims%'].rank(pct=True)
-    df['Pases hacia peligro %'] = df['Pass into Danger%'].rank(pct=True)
-    df['Calidad de posicionamiento'] = 1 - (df['Positioning Error'].rank(pct=True))
-    df['Pases'] = df['OP Passes'].rank(pct=True)
-    df['Éxito pases largos'] = df['Long Ball%'].rank(pct=True)
-    df['Éxito pases'] = df['Passing%'].rank(pct=True)
-    df['Éxito pases bajo presión'] = df['Pr. Pass%'].rank(pct=True)
-    df['% pases con zurdo'] = df['L/R Footedness%'] / 100
+    df['Long balls total']=(df['Long Balls']/df['Long Ball%'])*100
+    df['Long balls per pass']=df['Long balls total']/df['OP Passes']
+    df['% pases son largos']=df['Long balls per pass'].rank(pct=True)
+    df['Goles parados']=df['GSAA'].rank(pct=True)
+    df['OBV portero']=df['Goalkeeper OBV'].rank(pct=True)
+    df['Salidas de libero del portero']=df['GK Aggressive Dist.'].rank(pct=True)
+    df['Salidas de portero (centros)']=df['Claims%'].rank(pct=True)
+    df['Pases hacia peligro %']=df['Pass into Danger%'].rank(pct=True)
+    df['Calidad de posicionamiento']=1-(df['Positioning Error'].rank(pct=True))
+    df['Pases']=df['OP Passes'].rank(pct=True)
+    df['Éxito pases largos']=df['Long Ball%'].rank(pct=True)
+    df['Éxito pases']=df['Passing%'].rank(pct=True)
+    df['Éxito pases bajo presión']=df['Pr. Pass%'].rank(pct=True)
+    df['% pases con zurdo']=df['L/R Footedness%']/100
+
 
     df = df.loc[df['Name'] == j1]
     df = df.set_index('Name')
