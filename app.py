c827d6936ad03fcb2cf8b429afebd72e85552a42
cbkatiaa <katiaaacb@gmail.com>
2024-06-12 11:08:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,6 @@ def iqindportero(df, j1, pos):
             else:
                 colors.append(cmap(data_color[i]))
 
-        # Graficar las barras con los colores correctos
         ax.barh(x, y, color=colors, zorder=2, edgecolor='none')
         
         for c in ax.containers:
@@ -543,6 +542,218 @@ def iqindlateral(df, j1, pos):
 
     return fig
 
+def iqindcontencion(df, j1, pos):
+    c='white'
+    fig = plt.figure(frameon=False, edgecolor='#293A4A')
+    fig.set_figheight(18)
+    fig.set_figwidth(31)
+    sh=36
+    ax0 = plt.subplot2grid(shape=(sh, 7), loc=(0, 0), colspan=4, rowspan=6)
+    ax1 = plt.subplot2grid(shape=(sh, 7), loc=(6, 0), colspan=4, rowspan=9)
+    ax2 = plt.subplot2grid(shape=(sh, 7), loc=(15, 0), colspan=4, rowspan=3)
+    ax3 = plt.subplot2grid(shape=(sh, 7), loc=(18, 0), colspan=4, rowspan=3)
+    ax4 = plt.subplot2grid(shape=(sh, 7), loc=(21, 0), colspan=4, rowspan=9)
+    ax5 = plt.subplot2grid(shape=(sh, 7), loc=(30, 0), colspan=4, rowspan=3)
+    ax6 = plt.subplot2grid(shape=(sh, 7), loc=(2, 4), colspan=3, rowspan=1)
+    ax7 = plt.subplot2grid(shape=(sh, 7), loc=(3, 4), colspan=3, rowspan=12)
+    ax8 = plt.subplot2grid(shape=(sh, 7), loc=(18, 4), colspan=3, rowspan=1)
+    ax9 = plt.subplot2grid(shape=(sh, 7), loc=(19, 4), colspan=3, rowspan=12)
+    fig.subplots_adjust(left=0.1,
+                        bottom=0.1,
+                        right=0.9,
+                        top=0.9,
+                        wspace=0.05,
+                        hspace=0.3)
+
+    bar1=['Pases','% pases con zurdo','Éxito pases','Pases largos por pases','Progresiones al último tercio','Éxito pases largos','Éxito pases bajo presión']
+    bar2=['Presiones', 'Recuperación tras presión %','Contrapresiones', 'Entradas','Éxito 1vs1 defensivo','Intercepciones','Balones recuperados']
+    bar3=['Duelos aéreos ganados', 'Éxito duelos aéreos']
+    bar4=['Distancia conducciones','Regates','Éxito de regates','Faltas recibidas']
+    bar5=['xA','Centros','Pases al área','Tiros','Toques en el área']
+
+    t2='Pases'
+    t3='Presiones'
+    t4='xA'
+    t5='Pases progresivos'
+
+    ti1='Posesión'
+    ti2='Defensivo'
+    ti3='Juego aéreo'
+    ti4='Conducción'
+    ti5='Ofensivo'
+
+    c1 = 'white'
+    y1 = -3
+    vmax = 4
+    txs = 18
+    txs1 = 22
+    padr = -40
+
+    fig.add_artist(lines.Line2D([.57, .57], [1, 0.1], color='#293A4A', lw=5))
+    fig.add_artist(lines.Line2D([-.04, .57], [.78, 0.78], color='#293A4A', lw=5))
+    fig.add_artist(lines.Line2D([.57, .91], [.5, 0.5], color='#293A4A', lw=5))
+    #fig.add_artist(lines.Line2D([.6, .91], [.37, 0.37], color='#293A4A', lw=5))
+
+    plt.rcParams["font.family"] = "Century Gothic"
+
+
+    df['Press %']=df['Pressure Regains']/df['Pressures']
+    df['Éxito duelos aéreos']=df['Aerial Win%'].rank(pct=True)
+    df['Duelos aéreos ganados']=df['Aerial Wins'].rank(pct=True)
+    df['Acciones agresivas']=df['Aggressive Actions'].rank(pct=True)
+    df['Asistencias Torneo']=df['Assists'].rank(pct=True)
+    df['Asistencias Jugada Abierta p90']=df['OP Assists'].rank(pct=True)
+    df['Promedio minutos jugados por partido']=df['Minutes'].rank(pct=True)
+    df['Pases hacia atrás %']=df['Pass Backward%'].rank(pct=True)
+    df['Pases hacia adelante %']=df['Pass Forward%'].rank(pct=True)
+    df['Balones recuperados']=df['Ball Recoveries'].rank(pct=True)
+    df['Tiros bloqueados']=df['Blocks/Shot'].rank(pct=True)
+    #df['Éxito de centros']=df['player_season_box_cross_ratio'].rank(pct=True)
+    df['Acarreos']=df['Carries'].rank(pct=True)
+    df['Distancia de conducciones']=df['Carry Length'].rank(pct=True)
+    df['Acarreos exitosos']=df['Carry%'].rank(pct=True)
+    df['Éxito 1vs1 defensivo']=df['Tack/DP%'].rank(pct=True)
+    df['Dif éxito pases BP']=df['Pr. Pass% Dif.'].rank(pct=True)
+    #df['Salidas de portero (centros)']=df['player_season_clcaa'].rank(pct=True)
+    df['Despejes p90']=df['Clearances'].rank(pct=True)
+    df['Tiros convertidos en gol %']=df['Shooting%'].rank(pct=True)
+    df['Balones disputados ganados p90']=df['Counterpress Regains'].rank(pct=True)
+    df['Contrapresiones']=df['Counterpressures'].rank(pct=True)
+    df['Centros']=df['Successful Crosses'].rank(pct=True)
+    df['Éxito centros']=df['Crossing%'].rank(pct=True)
+    df['Pases en profundidad exitosos']=df['Deep Completions'].rank(pct=True)
+    df['Progresiones al último tercio']=df['Deep Progressions'].rank(pct=True)
+    df['Balones perdidos en disputa']=df['Dispossessed'].rank(pct=True)
+    df['Éxito de regates']=df['Dribble%'].rank(pct=True)
+    df['Regates']=df['Successful Dribbles'].rank(pct=True)
+    df['Pases hacia atrás 3/3 %']=df['F3 Pass Backward%'].rank(pct=True)
+    df['Pases hacia adelante 3/3 %']=df['F3 Pass Forward%'].rank(pct=True)
+    df['Pases último tercio']=df['OP F3 Passes'].rank(pct=True)
+    df['Balones recuperados campo rival']=df['Ball Recov. F2'].rank(pct=True)
+    df['Faltas cometidas']=df['Fouls'].rank(pct=True)
+    df['Faltas recibidas']=df['Fouls Won'].rank(pct=True)
+    df['Intercepciones p90']=df['Interceptions'].rank(pct=True)
+    df['% pases con zurdo']=df['L/R Footedness%'].rank(pct=True)
+    df['Éxito pases largos']=df['Long Ball%'].rank(pct=True)
+    df['Pases largos']=df['Long Balls'].rank(pct=True)
+    df['Tiros']=df['Shots'].rank(pct=True)
+    df['xG']=df['xG'].rank(pct=True)
+    df['xG por tiro']=df['xG/Shot'].rank(pct=True)
+    df['Goles (sin penales)']=df['NP Goals'].rank(pct=True)
+    df['Contribución de gol']=df['xG & xG Assisted'].rank(pct=True)
+    df['xG post-tiro']=df['PSxG'].rank(pct=True)
+    df['OBV total']=df['OBV'].rank(pct=True)
+    df['OBV pases']=df['Pass OBV'].rank(pct=True)
+    df['OBV tiros']=df['Shot OBV'].rank(pct=True)
+    df['OBV acciones defensivas']=df['DA OBV'].rank(pct=True)
+    df['OBV conducciones y regates']=df['D&C OBV'].rank(pct=True)
+    df['OBV portero']=df['Goalkeeper OBV'].rank(pct=True)
+    df['Distancia de pases bajo presión']=df['Pr. Pass Length Dif.'].rank(pct=True)
+    df['Despejes']=df['PAdj Clearances'].rank(pct=True)
+    df['Intercepciones']=df['PAdj Interceptions'].rank(pct=True)
+    df['Presiones']=df['PAdj Pressures'].rank(pct=True)
+    #df['Altura de presiones']=df['Average Pressure X'].rank(pct=True)
+    df['Entradas']=df['Tackles'].rank(pct=True)
+    df['Entradas e Intercepciones p90']=df['Tack&Int'].rank(pct=True)
+    df['Pases']=df['OP Passes'].rank(pct=True)
+    df['Pases dentro del área']=df['Passes Inside Box'].rank(pct=True)
+    df['Pases al área']=df['OP Passes Into Box'].rank(pct=True)
+    df['% pases bajo presión']=df['Passes Pressured%'].rank(pct=True)
+    df['Éxito pases']=df['Passing%'].rank(pct=True)
+    df['xA']=df['xG Assisted'].rank(pct=True)
+    df['Pases largos sin presión']=df['UPr. Long Balls'].rank(pct=True)
+    df['Pérdidas']=df['Turnovers'].rank(pct=True)
+    df['Toques en el área']=df['Touches In Box'].rank(pct=True)
+    df['Pases filtrados']=df['Throughballs'].rank(pct=True)
+    df['% tiros a puerta']=df['Shot Touch%'].rank(pct=True)
+    df['Presiones p90']=df['Pressures'].rank(pct=True)
+    df['Éxito pases bajo presión']=df['Pr. Pass%'].rank(pct=True)
+    df['Pases largos bajo presión']=df['Pr. Long Balls'].rank(pct=True)
+    df['Dif. distancia de pases bajo presión']=df['Pr. Pass% Dif.'].rank(pct=True)
+    df['Recuperación tras presión']=df['Pressure Regains'].rank(pct=True)
+    df['Recuperación tras presión %']=df['Press %'].rank(pct=True)
+    df['Presiones cancha rival']=df['Pressures F2'].rank(pct=True)
+    df['PS xG vs xG']=df['PSxG']/df['xG']
+    df['Calidad definición']=df['PS xG vs xG'].rank(pct=True)
+    df['Carry length total']=df['Carries']*df['Carry Length']
+    df['Distancia conducciones']=df['Carry length total'].rank(pct=True)
+    df['Long balls total']=(df['Long Balls']/df['Long Ball%'])*100
+    df['Long balls per pass']=df['Long balls total']/df['OP Passes']
+    df['Pases largos por pases']=df['Long balls per pass'].rank(pct=True)
+    df['% pases con zurdo']=df['L/R Footedness%']/100
+
+
+    df = df.loc[df['Name'] == j1]
+    df = df.set_index('Name').transpose()
+
+    ax0.axis('off')
+    ax6.axis('off')
+    ax7.axis('off')
+    ax8.axis('off')
+    ax9.axis('off')
+
+    def plot_bar_contencion(ax, bar_data, title):
+        #ax=ax.axis('off')
+        ax.set_facecolor(c1)
+        ax.set_xlim(0, 1)
+        ax.set_ylim(-1, len(bar_data))
+        ax.set_xticklabels([])
+        ax.yaxis.set_ticks_position('none')
+
+
+        df1 = df.reindex(bar_data).reindex(index=bar_data[::-1]).reset_index()
+        x = df1['index']
+        y = df1[j1]
+
+        data_color = [(x - 0) / (1 - 0) for x in y]
+        cmap = LinearSegmentedColormap.from_list('rg', ["darkred", "red", "salmon", "yellowgreen", "green", "darkgreen"], N=256)
+        
+        colors = [cmap(val) for val in data_color]
+        ax.barh(x, y, color=colors, zorder=2, edgecolor='none')
+        
+        for container in ax.containers:
+            labels = [(val * 100).astype(int) if val > .05 else "" for val in container.datavalues]
+            ax.bar_label(container, labels=labels, label_type='edge', color='w', size=txs, fontweight='bold', padding=padr)
+
+        for spine in ['top', 'bottom', 'left', 'right']:
+            ax.spines[spine].set_visible(False)
+
+        ax.set_yticklabels(df1['index'], color='black', size=20, fontname='Century Gothic', va='center')
+        ax.yaxis.set_tick_params(pad=15)
+        ax.set_title(title, color='black', size=22, x=0, y=0.93, ha='left', fontname='Century Gothic', fontweight='semibold')
+        ax.set_xticks([0.5])
+        ax.grid(color='grey', axis='x', which='major')
+
+
+    ax1.set_yticklabels('', color='black', size=20, fontname='Century Gothic', va='center')
+    ax1.yaxis.set_tick_params(pad=15)
+    ax1.set_xticks([])
+    ax1.grid(color='grey', axis='x', which='major', zorder=3)
+    ax1.text(0, y1, ti1, size=txs1, c='black', fontweight='semibold') 
+
+    ax1.set_yticklabels('', color='black', size=20, fontname='Century Gothic', va='center')
+    ax1.yaxis.set_tick_params(pad=15)
+    ax1.set_xticks([])
+    ax1.grid(color='grey', axis='x', which='major', zorder=3)
+    ax1.text(0, y1, t3, size=txs1, c='black', fontweight='semibold')
+
+
+    plot_bar_contencion(ax1, bar1, ti1)
+    plot_bar_contencion(ax2, bar2, ti2)
+    plot_bar_contencion(ax3, bar3, ti3)
+    plot_bar_contencion(ax4, bar4, ti4)
+    plot_bar_contencion(ax5, bar5, ti5)
+
+
+
+    j1 = j1.upper()
+    pos = pos.upper()
+    plt.figtext(0.05, 0.98, j1, c='#151616', fontsize=56, fontweight='bold', fontname='arial')
+    plt.figtext(0.05, 0.94, pos, c='#151616', fontsize=40, fontweight='bold', fontname='arial')
+
+
+    return fig
+
 
 
 st.title('Análisis de Jugadores')
@@ -584,7 +795,13 @@ posicion_funciones = {
     "Left Back": iqindlateral,
     "Left Wing Back": iqindlateral,
     "Right Back": iqindlateral,
-    "Right Wing Back": iqindlateral
+    "Right Wing Back": iqindlateral,
+    "Left Defensive Midfielder": iqindcontencion,
+    "Centre Defensive Midfielder": iqindcontencion,
+    "Right Defensive Midfielder": iqindcontencion,
+    "Left Centre Midfielder": iqindcontencion,
+    "Centre Midfielder": iqindcontencion,
+    "Right Centre Midfielder": iqindcontencion
 }
 
 if st.button("Generar Análisis"):
