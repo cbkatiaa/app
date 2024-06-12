1189bae951a46f29affe6775c22d0c8ecac36792
cbkatiaa <katiaaacb@gmail.com>
2024-06-11 19:49:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -60,59 +60,6 @@ def iqindportero(df, j1, pos):
     df = df.transpose()
 
     ax0.axis('off')
-    
-
-    def iqindportero(df, j1, pos):
-    c = 'white'
-    fig = plt.figure(frameon=False, edgecolor='#293A4A')
-    fig.set_figheight(18)
-    fig.set_figwidth(31)
-    sh = 16
-    ax0 = plt.subplot2grid(shape=(sh, 7), loc=(0, 0), colspan=4, rowspan=3)
-    ax1 = plt.subplot2grid(shape=(sh, 7), loc=(3, 0), colspan=4, rowspan=5)
-    ax2 = plt.subplot2grid(shape=(sh, 7), loc=(8, 0), colspan=4, rowspan=8)
-    ax6 = plt.subplot2grid(shape=(sh, 7), loc=(0, 4), colspan=3, rowspan=7)
-    ax7 = plt.subplot2grid(shape=(sh, 7), loc=(9, 4), colspan=3, rowspan=7)
-    fig.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.05, hspace=0.3)
-
-    bar1 = ['Goles parados', 'Calidad de posicionamiento', 'Salidas de portero (centros)', 'Salidas de libero del portero']
-    bar2 = ['Pases', '% pases con zurdo', 'Éxito pases', '% pases son largos', 'Éxito pases largos', 'Éxito pases bajo presión', 'Pases hacia peligro %']
-
-    ti1 = 'Acciones del portero'
-    ti2 = 'Posesión'
-    ti6 = 'Tiros en contra'
-    ti7 = 'Penales en contra'
-
-    c1 = 'white'
-    txs = 22
-    padr = -45
-
-    fig.add_artist(lines.Line2D([.57, .57], [1, 0.1], color='#293A4A', lw=5))
-    fig.add_artist(lines.Line2D([-.04, .57], [.76, 0.76], color='#293A4A', lw=5))
-    fig.add_artist(lines.Line2D([.57, .91], [.5, 0.5], color='#293A4A', lw=5))
-
-    plt.rcParams["font.family"] = "Century Gothic"
-
-    df['Long balls total'] = (df['Long Balls'] / df['Long Ball%']) * 100
-    df['Long balls per pass'] = df['Long balls total'] / df['OP Passes']
-    df['% pases son largos'] = df['Long balls per pass'].rank(pct=True)
-    df['Goles parados'] = df['GSAA'].rank(pct=True)
-    df['OBV portero'] = df['Goalkeeper OBV'].rank(pct=True)
-    df['Salidas de libero del portero'] = df['GK Aggressive Dist.'].rank(pct=True)
-    df['Salidas de portero (centros)'] = df['Claims%'].rank(pct=True)
-    df['Pases hacia peligro %'] = df['Pass into Danger%'].rank(pct=True)
-    df['Calidad de posicionamiento'] = 1 - (df['Positioning Error'].rank(pct=True))
-    df['Pases'] = df['OP Passes'].rank(pct=True)
-    df['Éxito pases largos'] = df['Long Ball%'].rank(pct=True)
-    df['Éxito pases'] = df['Passing%'].rank(pct=True)
-    df['Éxito pases bajo presión'] = df['Pr. Pass%'].rank(pct=True)
-    df['% pases con zurdo'] = df['L/R Footedness%'] / 100
-
-    df = df.loc[df['Name'] == j1]
-    df = df.set_index('Name')
-    df = df.transpose()
-
-    ax0.axis('off')
 
     def plot_bar_portero(ax, bar_data, title):
         ax.set_facecolor(c1)
@@ -131,20 +78,14 @@ def iqindportero(df, j1, pos):
         data_color = [(x - normmin) / (normmax - normmin) for x in data_color]
         cmap = LinearSegmentedColormap.from_list('rg', ["darkred", "red", "salmon", "yellowgreen", "green", "darkgreen"], N=256)
         cmap_invertida = LinearSegmentedColormap.from_list('rg', ["darkgreen", "green", "yellowgreen", "salmon", "red", "darkred"], N=256)
-
-        # Crear los colores para cada barra individualmente
-        colors = []
-        for i, label in enumerate(df1['index']):
-            if label == 'Pases hacia peligro %':
-                colors.append(cmap_invertida(data_color[i]))
-            else:
-                colors.append(cmap(data_color[i]))
-
-        # Graficar las barras con los colores correctos
+        for color in bar_data :
+            if bar_data == 'Pases hacia peligro %' or bar_data == 'Pass into Danger%':
+                    colors = cmap_invertida(data_color)
+            else: colors = cmap(data_color)
         ax.barh(x, y, color=colors, zorder=2, edgecolor='none')
         
         for c in ax.containers:
-            labels = [(v * 100).astype(int) if v > .05 else "" for v in c.datavalues]
+            labels = [(y * 100).astype(int) if y > .05 else "" for y in c.datavalues]
             ax.bar_label(c, labels=labels, label_type='edge', color='w', size=txs, fontweight='bold', padding=padr)
         for s in ['top', 'bottom', 'left', 'right']:
             ax.spines[s].set_visible(False)
