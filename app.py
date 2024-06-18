cc683e77538a66695a5fe572ed9eac9e2bb31db7
cbkatiaa <katiaaacb@gmail.com>
2024-06-18 09:31:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -343,7 +343,7 @@ def iqindlateral(df, j1, pos):
     ax1 = plt.subplot2grid(shape=(sh, 7), loc=(6, 0), colspan=4, rowspan=8)
     ax2 = plt.subplot2grid(shape=(sh, 7), loc=(15, 0), colspan=4, rowspan=7)
     ax3 = plt.subplot2grid(shape=(sh, 7), loc=(18, 0), colspan=4, rowspan=7)
-    ax4 = plt.subplot2grid(shape=(sh, 7), loc=(21, 0), colspan=4, rowspan=6)
+    ax4 = plt.subplot2grid(shape=(sh, 7), loc=(25, 0), colspan=4, rowspan=6)
     ax5 = plt.subplot2grid(shape=(sh, 7), loc=(30, 0), colspan=4, rowspan=9)
     ax6 = plt.subplot2grid(shape=(sh, 7), loc=(2, 4), colspan=4, rowspan=1)
     ax7 = plt.subplot2grid(shape=(sh, 7), loc=(3, 4), colspan=3, rowspan=12)
