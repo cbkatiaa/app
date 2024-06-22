884ce458ae22e7201ad2668485c6a1f46ae86911
cbkatiaa <katiaaacb@gmail.com>
2024-06-22 17:16:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1341,14 +1341,19 @@ if st.button("Generar Análisis"):
               #  st.pyplot(fig)  
             #except Exception as e:
              #   st.error(f"Error al generar el gráfico: {e}")
-            try:
+           try:
     # Lógica para pasar df_filtrado completo si no se selecciona un equipo específico
                 if equipo_seleccionado is not None:
                     df_filtrado_jugador_equipo = df_filtrado[(df_filtrado['Name'] == jugador_seleccionado) & (df_filtrado['Team'] == equipo_seleccionado)]
                     fig = funcion_grafico(df_filtrado_jugador_equipo, jugador_seleccionado, equipo_seleccionado, posicion_seleccionada)
-            else:
-                fig = funcion_grafico(df_filtrado, jugador_seleccionado, None, posicion_seleccionada)  # Pasar None si solo hay un equipo
+                else:
+                    fig = funcion_grafico(df_filtrado, jugador_seleccionado, None, posicion_seleccionada)  # Pasar None si solo hay un equipo
     
-    st.pyplot(fig)
-    else:
-        st.error(f"No hay una función de gráficos definida para la posición: {posicion_seleccionada}")
+                st.pyplot(fig)
+    
+            except Exception as e:
+                st.error(f"Error al generar el gráfico: {e}")
+    
+    #st.pyplot(fig)
+    #else:
+     #   st.error(f"No hay una función de gráficos definida para la posición: {posicion_seleccionada}")
